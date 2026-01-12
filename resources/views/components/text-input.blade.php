611824f08d5744436c78e870113b3f189beb8cab
Rizafii <firmansyah.riza6@smk.belajar.id>
2026-01-12 12:59:50 +0700

    fix: text input support wiremodel

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -9,34 +9,29 @@
         return 'Rp ' + formatted;
     },
     displayValue: '',
-    actualValue: '',
+    actualValue: @entangle($attributes->wire('model')),
     init() {
-        this.actualValue = this.$refs.input.value || '';
-        this.displayValue = this.actualValue ? this.formatCurrency(this.actualValue) : '';
+        this.actualValue = this.actualValue || '';
+        this.displayValue = this.actualValue ? this.formatCurrency(this.actualValue.toString()) : '';
+        this.$watch('actualValue', value => {
+            this.displayValue = value ? this.formatCurrency(value.toString()) : '';
+        });
     },
     updateValue(event) {
         let input = event.target.value;
         this.actualValue = input.replace(/[^\d]/g, '');
         this.displayValue = this.formatCurrency(this.actualValue);
-        this.$refs.hidden.value = this.actualValue;
-        this.$refs.input.value = this.actualValue;
     }
-}" x-init="init()">
+}">
     <input 
         type="text" 
         x-model="displayValue"
         @input="updateValue($event)"
         @disabled($disabled)
-        {{ $attributes->except(['wire:model', 'wire:model.live', 'wire:model.defer', 'wire:model.lazy'])->merge(['class' => 'border-gray-300    focus:border-indigo-500 :border-indigo-600 focus:ring-indigo-500 :ring-indigo-600 rounded-md shadow-sm']) }}
+        {{ $attributes->except(['wire:model', 'wire:model.live', 'wire:model.defer', 'wire:model.lazy'])->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}
         autocomplete="off"
     >
-    <input 
-        type="hidden" 
-        x-ref="hidden"
-        {{ $attributes->only(['wire:model', 'wire:model.live', 'wire:model.defer', 'wire:model.lazy', 'name']) }}
-    >
-    <input type="hidden" x-ref="input" value="{{ $attributes->get('value', '') }}">
 </div>
 @else
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300    focus:border-indigo-500 :border-indigo-600 focus:ring-indigo-500 :ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
 @endif
