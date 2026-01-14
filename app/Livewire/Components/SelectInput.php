97188cef6f832d98e347f4d3fd8ce97177381438
Rizafii <firmansyah.riza6@smk.belajar.id>
2026-01-14 11:28:38 +0700

    replace : wire live to alpine

--- app/Livewire/Components/SelectInput.php
+++ app/Livewire/Components/SelectInput.php
@@ -14,7 +14,6 @@ class SelectInput extends Component
     public $placeholder = '-- Pilih --';
     public $disabled = false;
     public $freetext = false;
-    public $search = '';
     public $open = false;
 
     public function mount($options = [], $placeholder = '-- Pilih --', $disabled = false, $freetext = false)
@@ -25,21 +24,9 @@ class SelectInput extends Component
         $this->freetext = $freetext;
     }
 
-    public function updatedValue($value)
-    {
-        // Jangan clear search saat value update
-    }
-
     public function getOptionsProperty()
     {
         return $this->processOptions($this->rawOptions);
-    }
-
-    public function updatedSearch($value)
-    {
-        if (!empty($value)) {
-            $this->open = true;
-        }
     }
 
     public function selectOption($optionValue, $optionLabel = null)
@@ -47,12 +34,9 @@ class SelectInput extends Component
         if ($this->freetext) {
             // Mode freetext: simpan label sebagai value
             $this->value = $optionLabel ?? $optionValue;
-            $this->search = $this->value;
         } else {
             // Mode normal: simpan value
             $this->value = $optionValue;
-            $selected = collect($this->options)->firstWhere('value', $optionValue);
-            $this->search = $selected ? $selected['label'] : '';
         }
         $this->open = false;
     }
@@ -61,41 +45,12 @@ class SelectInput extends Component
     {
         if (!$this->disabled) {
             $this->open = !$this->open;
-            if ($this->open && !$this->freetext) {
-                $this->search = '';
-            }
-        }
-    }
-
-    public function openDropdown()
-    {
-        if (!$this->disabled) {
-            $this->open = true;
         }
     }
 
     public function closeDropdown()
     {
         $this->open = false;
-
-        // Untuk freetext, pastikan value tersimpan dari search
-        if ($this->freetext && !empty($this->search)) {
-            $this->value = $this->search;
-        } else if (!$this->freetext) {
-            // Untuk non-freetext, reset search
-            $this->search = '';
-        }
-    }
-
-    public function getFilteredOptionsProperty()
-    {
-        if (empty($this->search)) {
-            return $this->options;
-        }
-
-        return collect($this->options)->filter(function ($option) {
-            return stripos($option['label'], $this->search) !== false;
-        })->values()->toArray();
     }
 
     public function getSelectedLabelProperty()
