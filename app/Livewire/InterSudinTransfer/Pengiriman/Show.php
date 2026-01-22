306c25c06174ca364555704612282e72d2223606
Rizafii <firmansyah.riza6@smk.belajar.id>
2026-01-22 10:22:39 +0700

    refactor : informasi dasar to modal, transfer sudin

--- app/Livewire/InterSudinTransfer/Pengiriman/Show.php
+++ app/Livewire/InterSudinTransfer/Pengiriman/Show.php
@@ -12,6 +12,18 @@ class Show extends Component
 
     public InterSudinTransfer $transfer;
 
+    public function mount()
+    {
+        // Dispatch transfer data to modal
+        $this->dispatch('setTransferData', [
+            'id' => $this->transfer->id,
+            'sudin_pengirim_id' => $this->transfer->sudin_pengirim_id,
+            'sudin_penerima_id' => $this->transfer->sudin_penerima_id,
+            'tanggal_transfer' => $this->transfer->tanggal_transfer?->format('Y-m-d'),
+            'notes' => $this->transfer->notes,
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.inter-sudin-transfer.pengiriman.show');
--- resources/views/livewire/inter-sudin-transfer/permintaan/show.blade.php
+++ resources/views/livewire/inter-sudin-transfer/permintaan/show.blade.php
@@ -4,6 +4,13 @@
             <div class="text-3xl font-semibold">Detail Transfer Permintaan</div>
         </div>
         <div class="text-right flex gap-2 justify-end">
+            <x-secondary-button @click="$dispatch('open-modal', 'transfer-information-modal')" type="button">
+                <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                Informasi
+            </x-secondary-button>
             <x-button variant="secondary" href="{{ route('transfer.permintaan.index') }}" wire:navigate>
                 Kembali
             </x-button>
@@ -11,33 +18,17 @@
     </div>
     <div>
         <x-card title="Detail Transfer">
-            <div class="">
-                <table class="table-auto w-full text-md space-y-2 ">
+            <div>
+                <table class="table-auto w-full text-md space-y-2">
                     <tr>
-                        <td class="font-semibold w-1/2">Tanggal Transfer</td>
-                        <td>{{ $transfer->tanggal_transfer?->format('d/m/Y') ?? '-' }}</td>
-                    </tr>
-                    <tr>
-                        <td class="font-semibold">Status</td>
+                        <td class="font-semibold w-1/2">Status</td>
                         <td><span
                                 class="bg-{{ $transfer->status_color }}-600 text-{{ $transfer->status_color }}-100 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">{{
     $transfer->status_text }}</span></td>
                     </tr>
                     <tr>
-                        <td class="font-semibold">Sudin Peminta (Anda)</td>
-                        <td>{{ $transfer->sudinPengirim?->name ?? '-' }}</td>
-                    </tr>
-                    <tr>
-                        <td class="font-semibold">Sudin Penerima (Tujuan)</td>
-                        <td>{{ $transfer->sudinPenerima?->name ?? '-' }}</td>
-                    </tr>
-                    <tr>
                         <td class="font-semibold">Pembuat</td>
                         <td>{{ $transfer->user?->name ?? '-' }}</td>
-                    </tr>
-                    <tr>
-                        <td class="font-semibold">Keterangan</td>
-                        <td>{{ $transfer->notes ?? '-' }}</td>
                     </tr>
                 </table>
             </div>
@@ -57,4 +48,7 @@
             </div>
         </x-card>
     </div>
+
+    <!-- Modal Components -->
+    <livewire:components.transfer-information-modal mode="show" :key="'transfer-info-modal-' . $transfer->id" />
 </div>
